fbd016907c485f2287a9541935c468c8af56a7e4
redcatbox@gmail.com <RedCatBox_0123>
2019-11-15 21:31:51 +0200

    tabs

--- Source/CamVol.Target.cs
+++ Source/CamVol.Target.cs
@@ -8,7 +8,7 @@ public class CamVolTarget : TargetRules
 	public CamVolTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-        DefaultBuildSettings = BuildSettingsVersion.V2;
-        ExtraModuleNames.Add("CamVol");
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.Add("CamVol");
 	}
 }
