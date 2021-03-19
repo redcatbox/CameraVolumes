906e1901e10a414ed7e456d7905786ad09515585
redcatbox <redcatbox@gmail.com>
2021-03-19 14:16:56 +0200

    upd

---
+++ Plugins/CameraVolumes/Source/CameraVolumes/Public/CameraVolumesRuntimeSettings.h
@@ -0,0 +1,13 @@
+// redbox, 2021
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "CameraVolumesRuntimeSettings.generated.h"
+
+UCLASS(Config = CameraVolumes, DefaultConfig)
+class CAMERAVOLUMES_API UCameraVolumesRuntimeSettings : public UObject
+{
+	GENERATED_BODY()
+};
